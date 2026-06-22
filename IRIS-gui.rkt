cb7e62aba6e208cc518903534497fc51cb993390
Muhammad <muhammadbinnauman2006@gmail.com>
2026-06-22 14:04:29 -0400

    added recursion function to make the 16 registers

--- IRIS-gui.rkt
+++ IRIS-gui.rkt
@@ -21,54 +21,49 @@
 (define content (new horizontal-panel%
                      [parent main-panel]))
 
-(define registers (new group-box-panel%
+(define register-panel (new group-box-panel%
                        [parent content]
                        [label "Registers"]))
 
-(define (make-registers
+(define (make-register-labels i)
+  (cond
+    [(= i 16) '()]
+    [else
+     (cons (new message%
+                [parent register-panel]
+                [label (format "R~a: 0" i)])
+           (make-register-labels (+ i 1)))]))
 
-(define r0-label (new message%
-                      [parent registers]
-                      [label "R0: 0"]))
+(define register-labels
+  (make-register-labels 0))
+
 
 (define (change-r0)
+  (define r0-label (list-ref register-labels 0))
   (send r0-label set-label
         (format "R0: ~a" r0-value))) ; the ~a puts r0-value instead of it, the ~a sort of works like an f string in python
-
-
-(define r1-label (new message%
-                      [parent registers]
-                      [label "R1: 0"]))
-
-(define r2-label (new message%
-                      [parent registers]
-                      [label "R2: 0"]))
-
-(define r3-label (new message%
-                      [parent registers]
-                      [label "R3: 0"]))
 
 
 (new button%
      [parent toolbar]
      [label "Run"]
      [callback
-      (lambda (button event) ; the button and event are two inputs the function takes, button: the button that was pressed, event: what should happen when the button is pressed
+      (lambda (button event) ; the button and event are two inputs the function takes, button: the button that was pressed, event: information about the click
         (displayln "Run Clicked"))]) ; prints this string when the button is pressed. added this for now to test, can be removed later
 
 (new button%
      [parent toolbar]
      [label "Step"]
      [callback
-      (lambda (button event) ; the button and event are two inputs the function takes, button: the button that was pressed, event: what should happen when the button is pressed
-        (set r0-value (+ r0-value 1))
+      (lambda (button event) ; the button and event are two inputs the function takes, button: the button that was pressed, event: information about the click
+        (set! r0-value (+ r0-value 1))
         (change-r0))])
 
 (new button%
      [parent toolbar]
      [label "Reset"]
      [callback
-      (lambda (button event) ; the button and event are two inputs the function takes, button: the button that was pressed, event: what should happen when the button is pressed
+      (lambda (button event) ; the button and event are two inputs the function takes, button: the button that was pressed, event: information about the click
         (displayln "Reset Clicked"))]) ; prints this string when the button is pressed. added this for now to test, can be removed later
 
 

ae576bf8bfd6c08ad8cef718575b5ecab09c8e4b
davew <davew>
2026-06-26 15:48:05 -0400

    Untested but all are written for HERA-api

--- HERA-api.rkt
+++ HERA-api.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require racket/random)
-(provide get-PC step! get-flags get-registers get-data get-code)
+(provide get-PC step! get-flags get-register get-data get-code get-code-asm)
+(provide load-data load-code)
 
 
 ; Just mockup for now ... all provided functions can be called, but
@@ -11,13 +12,17 @@
 (define verbose #f)
 (define PC 0)
 (define flags       (make-vector 5       #f))
-(define registers   (list->vector (random-sample (list 0 1 2 7 12 65535) 16)))
-                    (vector-set! registers 0 0)
-(define memory-data (list->vector (random-sample (list 0 1 2 3 4 7 12 17 65535 65534 (random -4 48) (random -4 48) (random -4 48) (random -4 48))
-                                                 memsize)))
-(define memory-code (list->vector (random-sample (list #xA123 #xA121 #xA321 #xA221)
-                                                 memsize)))
+(define registers   (make-vector 16       0))
+(define memory-data (make-vector memsize  0))
+(define memory-code (make-vector memsize  0))
 
+
+(define (hera-val? v)
+  (and (integer? v) (<= 0 v) (< v wordlim)))
+(define (hera-addr? a)
+  (and (integer? a) (<= 0 a) (< a memsize)))
+(define (hera-reg-num? r)
+  (and (integer? r) (<= 0 r) (< r 16)))
 
 
 (define/contract (get-PC)  ; return the program counter
@@ -29,8 +34,34 @@
     (vector-set! registers 1 (modulo (+ (vector-ref registers 2) (vector-ref registers 3)) wordlim)))
   (set! PC (modulo (+ PC 1) memsize)))
 
-(define/contract (get-flags)     (-> vector?) flags)
-(define/contract (get-registers) (-> vector?) registers)
-(define/contract (get-data)      (-> vector?) memory-data)
-(define/contract (get-code)      (-> vector?) memory-code)
+(define/contract (get-flags)      (-> vector?) flags)
+(define/contract (get-register r)            ; -->
+  (->                          hera-reg-num? hera-val?)
+  (vector-ref registers   r))
+
+(define/contract (get-data dadr)      ; -->
+  (->                      hera-addr? hera-val?)
+  (vector-ref memory-data dadr))
+
+(define/contract (get-code iadr)      ; -->
+  (->                      hera-addr? hera-val?)
+  (vector-ref memory-code iadr))
+(define/contract (get-code-asm iadr)      ; -->
+  (->                          hera-addr? string?)
+  "ADD(R3, R5,R2) // e.g.")
   
+
+; (set! registers (list->vector (random-sample (list 0 1 2 7 12 65535) 16)))
+;  (vector-set! registers 0 0)
+
+(define/contract (load-data filename)  ; -->
+  (->                       string?     void?)
+  (set! memory-data
+        (list->vector (random-sample (list 0 1 2 3 4 7 12 17 65535 65534 (random -4 48) (random -4 48) (random -4 48) (random -4 48))
+                                     memsize))))
+
+(define/contract (load-code filename)  ; -->
+  (->                       string?     void?)
+  (set! memory-code
+        (list->vector (random-sample (list #xA123 #xA121 #xA321 #xA221)
+                                     memsize))))
